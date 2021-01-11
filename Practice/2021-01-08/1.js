6f70eebbbd433c68e7f69821b5c3ab7075db980b
Aashruti Prajapati <69380559+AashrutiPrajapati@users.noreply.github.com>
2021-01-11 16:36:46 +0530

    Move file from main to javascript

---
+++ Practice/2021-01-08/1.js
@@ -0,0 +1,33 @@
+console.log("Hellow World...");
+
+var firstName='Rasi';
+var age=20;
+var fullAge=true;
+var lastname;
+lastname='Prajapati';
+
+console.log(firstName,age,fullAge,lastname);
+
+//we can not use kewords as a variable name.
+/*
+This is the multiple line comments.
+...
+*/
+console.log(firstName + ' ' + lastname);
+
+//variable mutation.
+/* age = 'twenty';
+console.log(firstName + ' is ' + age + ' year old.');
+alert(firstName + ' ' + lastname);
+
+var job = prompt('Whis is the Job name ?');
+console.log('job name is ' + job );
+*/
+
+//Basic operators
+var a = 10 + 5;
+console.log(a);
+var b = a - 5;
+console.log(b);
+
+console.log(typeof a);

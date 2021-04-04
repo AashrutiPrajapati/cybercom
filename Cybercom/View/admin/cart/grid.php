bfb8d57cbe0dd66063e185612d2a4de9b3bb0ce1
Aashruti Prajapati <69380559+AashrutiPrajapati@users.noreply.github.com>
2021-04-04 23:54:34 +0530

    View folder files are updated

--- Cybercom/View/admin/cart/grid.php
+++ Cybercom/View/admin/cart/grid.php
@@ -1,23 +1,29 @@
 <?php 
+    $cart = $this->getCart();
     $cartItems = $this->getCart()->getItems(); 
     $customers = $this->getCustomers();
+    $customerBillingAddress = $this->getCustomerBillingAddress();
+    $customerShippingAddress = $this->getCustomerShippingAddress();
     // echo "<pre>";
-    // print_r($customers);
+    // print_r($customerShippingAddress); die;
+
+    // echo "<pre>";
+    // print_r($this->getCart()->getCustomer());
 ?>
 
 <h3 class="text-center mt-5">Cart Item Records</h3>
+    <form method='POST' action="<?php echo $this->getUrl('update'); ?>" id='cartForm'>
 <div class="container-fluid mb-3" style="margin-top:40px; margin-left:61%">
     <a href="<?php echo $this->getUrl('grid','admin\product');?>" class="btn btn-success font-weight-bold"> Back to Item</a>
     <input type = "submit" value="Update Cart" class="btn btn-success font-weight-bold">
 </div>
 <div class="container-fluid text-center">
-    <form method='POST' action="<?php echo $this->getUrl('update'); ?>" id='cartForm'>
         
         <div class="container-fluid d-flex justify-content-left mb-4"  >
             <select name="customer" class="form-control col-3">
                 <option>Select Customer</option>
                 <?php foreach ($customers->getData() as $key => $customer) : ?>
-                    <option value="<?php echo $customer->customerId?>" name='customer'><?php echo $customer->firstName;?></option>
+                    <option value="<?php echo $customer->customerId?>" name='customer' <?php if($customer->customerId == $cart->customerId) {echo "selected";}?>><?php echo $customer->firstName;?></option>
                 <?php endforeach; ?>
             </select>
             <button type="button" onclick="selectCustomer();" class="btn btn-info font-weight-bold ml-3">Go</button>
@@ -53,72 +59,129 @@
                     </tr>
             <?php } endif; ?>
         </table>  
-    </form> 
+     
     <div class="row">
-    <div class="col-lg-6">
-    <table class="table table-bordered pr-3">
-        <tr>
-            <td colspan=2 class="font-weight-bold">Billing Address</td>
-        </tr>
-        <tr>
-            <td>Address</td>
-            <td><input type="text" name="billing[address]" value="<?php echo $billing->address; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>City</td>
-            <td><input type="text" name="billing[city]" value="<?php echo $billing->city; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>State</td>
-            <td><input type="text" name="billing[state]" value="<?php echo $billing->state; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Zipcode</td>
-            <td><input type="text" name="billing[zipCode]" value="<?php echo $billing->zipCode; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Country</td>
-            <td><input type="text" name="billing[country]" value="<?php echo $billing->country; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td colspan=2><input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"></td>
-        </tr>
-            
-    </table>
-    </div>
+        <div class="col-lg-6">
+            <table class="table table-bordered pr-3">
+                <tr>
+                    <td colspan=2 class="font-weight-bold">Billing Address</td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td><input type="text" name="billing[address]" value="<?php echo $customerBillingAddress->address; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td><input type="text" name="billing[city]" value="<?php echo $customerBillingAddress->city; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>State</td>
+                    <td><input type="text" name="billing[state]" value="<?php echo $customerBillingAddress->state; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Zipcode</td>
+                    <td><input type="text" name="billing[zipcode]" value="<?php echo $customerBillingAddress->zipcode; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td><input type="text" name="billing[country]" value="<?php echo $customerBillingAddress->country; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="button" onclick="saveBillingAddress();" class="btn btn-success font-weight-bold">Save</button>
+                        <!-- <input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"> -->
+                    </td>
+                    <td><input type="checkbox" name="saveBillingInAddressBook" class="font-weight-bold" value="1"> Save in Address Book</td>
+                </tr>
+                    
+            </table>
+        </div>
 
-    <div class="col-lg-6">
-    <table class="table table-bordered ">
-        <tr>
-            <td colspan=2 class="font-weight-bold">Shipping Address</td>
-        </tr>
-        <tr>
-            <td>Address</td>
-            <td><input type="text" name="shipping[address]" value="<?php echo $shipping->address; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>City</td>
-            <td><input type="text" name="shipping[city]" value="<?php echo $shipping->city; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>State</td>
-            <td><input type="text" name="shipping[state]" value="<?php echo $shipping->state; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Zipcode</td>
-            <td><input type="text" name="shipping[zipCode]" value="<?php echo $shipping->zipCode; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td>Country</td>
-            <td><input type="text" name="shipping[country]" value="<?php echo $shipping->country; ?>" class="form-control"></td>
-        </tr>
-        <tr>
-            <td colspan=2><input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"></td>
-        </tr>
-            
-    </table>
-    </div>
-</div>   
+        <div class="col-lg-6">
+            <table class="table table-bordered ">
+                <tr>
+                    <td colspan=2 class="font-weight-bold">Shipping Address</td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td><input type="text" name="shipping[address]" value="<?php echo $customerShippingAddress->address; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td><input type="text" name="shipping[city]" value="<?php echo $customerShippingAddress->city; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>State</td>
+                    <td><input type="text" name="shipping[state]" value="<?php echo $customerShippingAddress->state; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Zipcode</td>
+                    <td><input type="text" name="shipping[zipcode]" value="<?php echo $customerShippingAddress->zipcode; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td><input type="text" name="shipping[country]" value="<?php echo $customerShippingAddress->country; ?>" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>
+                    <button type="button" onclick="saveShippingAddress();" class="btn btn-success font-weight-bold">Save</button>
+                    
+                    <!-- <input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"></td> -->
+                    <td><input type="checkbox" value="1" name="sameAsBilling"> Same As Billing   <input type="checkbox" name="saveShippingInAddressBook" class="font-weight-bold" value="1"> Save in Address Book</td>
+                </tr>
+                    
+            </table>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-lg-3">
+            <table class="table table-bordered">
+                <tr>
+                    <td class="font-weight-bold">Payment Method</td>
+                </tr>
+                <tr>
+                    <td>Credit Card <input type="radio" value="credit cart"><br>
+                        Debit Card <input type="radio" value="debit cart"><br>
+                        PayPal <input type="radio" value="paypal"><br>
+                        COD <input type="radio" value="cod">
+                    </td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-lg-3">
+            <table class="table table-bordered">
+                <tr>
+                    <td class="font-weight-bold">Shipping Method</td>
+                </tr>
+                <tr>
+                    <td>Express Delivery(1 Day) <input type="radio"><br>
+                        Paltinum Delivery(3 Days) <input type="radio"><br>
+                        Regular Delivery(7 Days) <input type="radio">
+                    </td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Save" class="btn btn-success font-weight-bold" style="padding:5px 20px"></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-lg-6">
+            <table class="table table-bordered">
+                <tr>
+                    <td>Base Total : <br>
+                        Shipping Charges : <br>
+                        Coupon : 
+                    </td>
+                </tr>
+                <tr>
+                    <td class="font-weight-bold">GRAND TOTAL : </td>
+                </tr>
+            </table>
+        </div>
+    </div> 
+    </form> 
 </div>
 
 <script style="text\javascript">
@@ -126,5 +189,18 @@
         var form = document.getElementById('cartForm');
         form.setAttribute('Action', '<?php echo $this->getUrl('selectCustomer'); ?>');
         form.submit();
+    }
+
+    function saveBillingAddress(){
+        var form = document.getElementById('cartForm');
+        form.setAttribute('Action', '<?php echo $this->getUrl('saveBillingAddress'); ?>');
+        form.submit();   
+    }
+
+    function saveShippingAddress(){
+        var form = document.getElementById('cartForm');
+        form.setAttribute('Action', '<?php echo $this->getUrl('saveShippingAddress'); ?>');
+        form.submit();
+        
     }
 </script>
--- Cybercom/View/admin/customer/grid.php
+++ Cybercom/View/admin/customer/grid.php
@@ -1,75 +1,34 @@
-<?php 
-$customers = $this->getCustomers();
-$data = [];
-if($customers){
-    foreach($customers as $key=>$value){
-        $data = $value->getData();
-        break;
-    }   
-}
+<?php $customers=$this->getCustomers();
 ?>
 
 <h2 style="text-align: center;">Customer Records</h2>
-<a href="<?php echo $this-> getUrl('edit') ?>" class="btn btn-success ml-5" role="button"><i class="fa fa-sm fa-plus"></i> Add Records</a><br><br>
-<table border="2px" cellpadding="8px" width="40%" class="ml-5" style="border-collapse:collapse">
-    <?php if(!$customers): ?>
-    <tr>
-        <td>No Record Found</td>
-    </tr>
-    <?php else: ?>
+<a href="<?php echo $this->getUrl('edit') ?>" class="btn btn-success ml-5" role="button"><i class="fa fa-sm fa-plus"></i> Add Records</a><br><br>
+<table border="2px" cellpadding="3px" class="ml-4" style="border-collapse:collapse">
     <tr class="bg-dark text-white">
-        <?php foreach($data as $key=>$value){ ?>
-            <th class="gridth"><?php echo $key?></th>
-        <?php } ?>
-        <th class="gridth" colspan='2'>Action</th>
+        <th>Customer_Id</th>
+        <th>Group_Id</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Mobile</th>
+        <th>Password</th>
+        <th>Status</th>
+        <th>Address</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Zipcode</th>
+        <th>Country</th>
+        <th>AddressType</th>
+        <th>CreatedDate</th>
+        <th>UpdatedDate</th>
+        <th colspan='2'>Action</th>
     </tr>
-
-    <?php foreach ($customers as $key => $value) { ?>
-        <?php if($value->addressType == 'Shipping'){continue;} ?>
-    <tr class="gridtr">
-        <?php foreach($data as $key1=>$value1){ ?>
-        <td class="gridtd"><?php echo $value->$key1; ?></td>
-        <?php } ?>
-        <td class="gridtd" colspan=2>
-            <a href = "<?php echo $this->getUrl('edit',NULL,['id'=>$value->customerId,'firstName'=>$value->firstName,'lastName'=>$value->lastName,'email'=>$value->email,'password'=>$value->password,'status'=>$value->status]); ?>" class="btn btn-Success">
-                Update
-            </a>
-            <a href = "<?php  echo $this->getUrl('delete',NULL,['id'=>$value->customerId]); ?>"class="btn btn-Danger">
-                Delete
-            </a>
-        </td>
-    </tr>
-    <?php } endif; ?>
-</table>
-
-<?php /*
-<html>
-<head>
-    <title>Customer Records</title>
-    <meta charset="utf-8">  
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-    <h3 class="text-center mt-5">Customer Records</h3>
-    <div class="container-fluid" style="margin-left:5% ;margin-bottom:20px"><a href="http://localhost/Cybercom/index.php?c=customer&a=edit" class="btn btn-success font-weight-bold"><i class="fa fa-sm fa-plus"></i> Add Customer</a></div>
-    <div class="container-fluid text-center d-flex justify-content-center ">
-    <table class="table table-bordered" style="width:90%">
-        <tr class="bg-dark text-white">
-                <th>Customer_Id</th>
-                <th>Group_Id</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Mobile</th>
-                <th>Password</th>
-                <th>Status</th>
-                <th>CreatedDate</th>
-                <th>UpdatedDate</th>
-                <th>Action</th>
+    <?php if(!$customers): ?>
+        <tr>
+            <td>No records!</td>
         </tr>
-<?php
-    $data=$this->getCustomers();
-    foreach ($data as $val) {?>
+        <?php else: ?> 
+           <?php foreach ($customers->getData() as $key=>$val) :?>
          <tr>
                 <td><?php echo $val->customerId; ?></td>
                 <td><?php echo $val->groupId; ?></td>
@@ -79,14 +38,24 @@ if($customers){
                 <td><?php echo $val->mobile; ?></td>
                 <td><?php echo $val->password; ?></td>
                 <td><?php echo $val->status; ?></td>
+                <?php //if($value->addressType == 'Billing'): 'continue';?>
+                <td><?php echo $val->address; ?></td>
+                <td><?php echo $val->city; ?></td>
+                <td><?php echo $val->state; ?></td>
+                <td><?php echo $val->zipCode; ?></td>
+                <td><?php echo $val->country; ?></td>
+                <td><?php echo $val->addressType; ?></td>
+                <?php //endif;?>
                 <td><?php echo $val->createdDate; ?></td>
                 <td><?php echo $val->updatedDate; ?></td>
-                <td><a href='index.php?c=customer&a=edit&id=<?php echo $val->customerId; ?>' class="btn btn-info btn-sm">Edit</a>
-                    <a href='index.php?c=customer&a=delete&id=<?php echo $val->customerId; ?>' class="btn btn-danger btn-sm">Delete</a></td>
+                <td><a href='<?php echo $this->getUrl('edit', null, ['id' => $val->customerId]) ?>' class="btn btn-info btn-sm">Edit</a>
+                    <a href='<?php echo $this->getUrl('delete', null, ['id' => $val->customerId]) ?>' class="btn btn-danger btn-sm">Delete</a></td>
             </tr>
-        <?php } ?>
-    </table> 
-    </div>
-</body>
-</html>
-*/?>
+            <?php endforeach;?>
+    <?php endif;?>
+
+        
+
+
+
+</table>

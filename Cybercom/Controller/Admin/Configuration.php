4430a8ba5010042b18a55e22f2c7dc9e0d9f2e61
Aashruti Prajapati <69380559+AashrutiPrajapati@users.noreply.github.com>
2021-04-05 22:58:15 +0530

    added configuration files

---
+++ Cybercom/Controller/Admin/Configuration.php
@@ -0,0 +1,88 @@
+<?php 
+namespace Controller\Admin;
+
+class Configuration extends \Controller\Core\Admin  
+{
+    public function gridAction()
+    {
+        $layout = $this->getLayout();
+        $layout->setTemplate('./View/core/layout/oneColumn.php');
+        $grid = \Mage::getBlock('Block\Admin\Configuration\Grid');
+        $content = $layout->getChild('content');
+        $content->addChild($grid);
+        echo $this->toHtmlLayout();
+
+    }
+
+    public function editAction()
+    {
+        try{
+            $layout = $this->getLayout(); 
+            $content = $layout->getChild('content');
+            $config = \Mage::getModel('Model\Configuration');
+
+            if ($id = $this->getRequest()->getGet('id')){   
+                $config = $config->load($id);
+            }
+            $edit =  \Mage::getBlock('Block\Admin\Configuration\Edit')->setTableRow($config);
+            $content->addChild($edit);
+            echo $this->toHtmlLayout();
+        }
+        catch (\Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function saveAction() {
+        try{
+            $config = \Mage::getModel('Model\Configuration');
+
+            if(!$this->getRequest()->isPost()){
+                throw new \Exception ("Invalid Request");
+            }
+
+            if ($id = $this->getRequest()->getGet('id')) {
+                $config = $config->load($id);
+
+                if (!$config){
+                    throw new \Exception ("Records not found.");
+                }
+            }
+            else {
+                $config->createdDate = date("Y-m-d H:i:s");
+            }
+            $configData = $this->getRequest()->getPost('config'); 
+            $config->setData($configData);
+            $config->save();
+            $this->getMessage()->setSuccess('Record Inserted Successfully.');
+        }
+        catch(\Exception $e){
+            $this->getMessage()->setFailure($e->getMessage());
+        }
+        $this->redirect('grid',null,null,true);
+    }
+
+    public function deleteAction()
+    {
+        try{
+            $id = $this->getRequest()->getGet('id');
+            if(!$id){
+                throw new \Exception("Invalid ID.");    
+            }
+            $config = \Mage::getModel('Model\Configuration');
+            $config->load($id);
+            
+            if($config->delete()) {
+                $this->getMessage()->setSuccess('Record Deleted Successfully.');
+            }
+            else {
+                $this->getMessage()->setFailure('Unable to Delete Record.');
+            }
+        }
+        catch (\Exception $e) {
+            $this->getMessage()->setFailure($e->getMessage());
+        }  
+        $this->redirect('grid',null,null,true);
+        
+    }
+}
